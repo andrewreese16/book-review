1dff67a5f03091b7c2a233810269cd131139706b
andrewreese16 <andrewreese1616@gmail.com>
2024-09-16 17:59:38 -0400

    styled more and added background image on home page

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const session = require("express-session");
-const path = require('path')
+const path = require("path");
 
 app.set("view engine", "ejs");
 
@@ -28,7 +28,7 @@ mongoose.connection.on("connected", () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
